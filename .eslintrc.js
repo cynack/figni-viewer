8af7029afa50c6ac657c3def269e09abe3f30dd9
Kohei Futatsuka <futahei1108@icloud.com>
2021-10-27 12:49:06 +0900

    model-viewer test

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 13,
+    'sourceType': 'module',
+  },
+  'rules': {
+    'require-jsdoc': 0,
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import {ModelViewerElement} from '@google/model-viewer';
+
+class FigniViewerElement extends ModelViewerElement {
+  constructor() {
+    super();
+
+    console.log('aaa');
+  }
+}
+
+customElements.define('figni-viewer', FigniViewerElement);

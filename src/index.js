7cde62b339084038fbce9ab7f872565a6db8612f
Kohei Futatsuka <futahei1108@icloud.com>
2021-10-27 17:04:22 +0900

    formatted

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import {ModelViewerElement} from '@google/model-viewer';
 class FigniViewerElement extends ModelViewerElement {
   constructor() {
     super();
+  }
+  connectedCallback() {
+    super.connectedCallback();
 
-    console.log('Using FigniViewer!');
+    console.log(this.shadowRoot);
+
+    // 値の取得
+    const modelId = this.getAttribute('model_id');
+    const accessId = this.getAttribute('access_id');
+    console.log(modelId);
+    console.log(accessId);
+
+    // TODO: figni-api からモデルの情報をとってくる
+    const modelSrc = 'https://storage.googleapis.com/cynack-norma/sample/grill_b.glb';
+
+    // 値の設定
+    this.setAttribute('src', modelSrc);
+    this.setAttribute('camera-controls', true);
   }
 }
 

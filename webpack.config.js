29b31d5bd772abce79d06b2292ae5194f9ac63db
Kohei Futatsuka <futahei1108@icloud.com>
2021-11-24 11:45:43 +0900

    split css

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,18 @@ const config = {
           },
         ],
       },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: !isProduction,
+            },
+          },
+        ],
+      },
     ],
   },
   optimization: {

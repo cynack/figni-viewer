5a901666115cbdf6e5ef6e47d1849bbdcb5ed26f
Futahei <futahei1108@icloud.com>
2022-01-25 18:00:02 +0900

    update error message

--- src/error.js
+++ src/error.js
@@ -7,15 +7,15 @@ export function getErrorMessage(err) {
   if (err.response && err.response.data) {
     switch (err.response.data.error) {
       case 'ErrItemNotFound': {
-        return '商品が見つかりませんでした'
+        return '商品が見つかりませんでした(ERR_ITEM_NOT_FOUND)'
       }
       case 'ErrInvalidClientToken': {
-        return 'トークンが無効です'
+        return 'トークンが無効です(ERR_INVALID_CLIENT_TOKEN)'
       }
       case 'ErrQuotaLimitReached': {
-        return 'API利用回数が上限に達しました'
+        return 'API利用回数が上限に達しました(ERR_QUOTA_LIMIT_REACHED)'
       }
     }
   }
-  return 'エラーが発生しました'
+  return 'エラーが発生しました(ERR_UNKNOWN)'
 }

9251b06afacb1200c130686b05aa1df4bdfe4fd6
Kohei Futatsuka <futahei1108@icloud.com>
2022-05-25 18:02:34 +0900

    fix helppage mesure

--- src/figni-viewer.js
+++ src/figni-viewer.js
@@ -9,7 +9,7 @@ import {
   LOADING_ANIMATION,
   LOADING_ANIMATION_RING,
   MOVE_AR_CONTENT_ANIMATION,
-  ROTATE_AR_CONTENT_ANIMATION,
+  ROTATE_AR_CONTENT_ANIMATION
 } from './animation'
 import { getError } from './error'
 import './style.scss'
@@ -26,7 +26,7 @@ import {
   SVG_HELP_UNKNOWN_ICON,
   SVG_INTERACTION_PROMPT,
   SVG_TOGGLE_VISIBLE_HOTSPOT_BUTTON_OFF,
-  SVG_TOGGLE_VISIBLE_HOTSPOT_BUTTON_ON,
+  SVG_TOGGLE_VISIBLE_HOTSPOT_BUTTON_ON
 } from './svg'
 
 const OBSERBED_ATTRIBUTES = [
@@ -570,6 +570,9 @@ export default class FigniViewerElement extends HTMLElement {
       const openedPage = this.#openedHelpPages.pop()
       openedPage.page.style.left = '100%'
       this.#figniViewerBase.endMesureHelpPage(openedPage.name)
+      if (this.#openedHelpPages[this.#openedHelpPages.length - 1]) {
+        this.#figniViewerBase.startMesureHelpPage(this.#openedHelpPages[this.#openedHelpPages.length - 1].name)
+      }
       setTimeout(() => {
         openedPage.page.remove()
       }, 300)

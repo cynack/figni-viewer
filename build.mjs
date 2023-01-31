14de775cac26cb174ebb6581762daaab488e6417
Kohei Futatsuka <futahei1108@icloud.com>
2023-01-31 16:21:04 +0900

    remove webpack & add esbuild

---
+++ build.mjs
@@ -0,0 +1,20 @@
+import esbuild from 'esbuild'
+import { sassPlugin } from 'esbuild-sass-plugin'
+
+const options = {
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  minify: true,
+  outfile: 'dist/figni-viewer.min.js',
+  define: {
+    VERSION: JSON.stringify(process.env.VERSION || ''),
+    API_BASE: JSON.stringify('https://api.figni.io/api'),
+    WEBSOCKET_BASE: JSON.stringify('wss://api.figni.io/ws'),
+    TRANSLATIONS_FILE: JSON.stringify(
+      'https://storage.googleapis.com/cynack/figni/viewer/translations.yml'
+    ),
+  },
+  plugins: [sassPlugin({ type: 'style' })],
+}
+
+await esbuild.build(options)
---
+++ watch.mjs
@@ -0,0 +1,78 @@
+import esbuild from 'esbuild'
+import { sassPlugin } from 'esbuild-sass-plugin'
+
+const warningLog = (warning) => {
+  warning.forEach((warn) => {
+    console.error('warning: ', warn.text)
+    console.error('detail: ', warn.detail)
+    console.error(
+      'path: ',
+      `${warn.location.file}:${warn.location.line}:${warn.location.column}`
+    )
+    console.error(' -> ', warn.location.lineText)
+  })
+}
+const errorLog = (errors) => {
+  errors.forEach((err) => {
+    console.error('error: ', err.text)
+    console.error(
+      'path: ',
+      `${err.location.file}:${err.location.line}:${err.location.column}`
+    )
+    console.error(' -> ', err.location.lineText)
+  })
+}
+
+const ctx = await esbuild.context({
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  minify: true,
+  color: true,
+  sourcemap: true,
+  outfile: 'test/js/figni-viewer.min.js',
+  define: {
+    VERSION: JSON.stringify(process.env.VERSION || ''),
+    API_BASE: JSON.stringify('https://api.stg.figni.io/api'),
+    WEBSOCKET_BASE: JSON.stringify('wss://api.stg.figni.io/ws'),
+    TRANSLATIONS_FILE: JSON.stringify('translations.yml'),
+  },
+  plugins: [
+    sassPlugin({ type: 'style' }),
+    {
+      name: 'on-end',
+      setup(build) {
+        build.onEnd((result) => {
+          console.log('----------------------------')
+          if (result.errors.length > 0) {
+            console.error(new Date().toLocaleString(), ' watch build failed ')
+            if (result.warnings.length) {
+              result.warnings.forEach((warn) => {
+                warningLog(warn)
+              })
+            }
+            if (result.errors.length) {
+              result.errors.forEach((err) => {
+                errorLog(err)
+              })
+            }
+          } else {
+            console.log(new Date().toLocaleString(), ' watch build succeeded ')
+            if (result.warnings.length) {
+              result.warnings.forEach((warn) => {
+                warningLog(warn)
+              })
+            }
+          }
+        })
+      },
+    },
+  ],
+})
+
+try {
+  console.log('============================')
+  console.log('Compile start... ', new Date().toLocaleDateString())
+  await ctx.watch()
+} catch (err) {
+  console.error(JSON.stringify(err, null, 2))
+}
